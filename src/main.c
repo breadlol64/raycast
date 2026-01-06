29ef1cfd857df6ec805f7ac3e37c56ac38c6361a
breadlol64 <breadlol64@gmail.com>
2026-01-06 11:46:59 +0200

    raycasting

--- src/main.c
+++ src/main.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 
-#define CELL_W 64.0f
+#define CELL_W 50.0
+#define SCREEN_W 800
+#define SCREEN_H 600
 
-float playerX, playerY;
-float playerDirX, playerDirY;
-float playerRot;
-float moveSpeed;
-float rotationSpeed;
+double playerX, playerY;
+double playerDirX, playerDirY;
+double playerRot;
+double moveSpeed;
+double rotationSpeed;
+double planeX, planeY;
+double planeRot;
 
 int map[8][8] = {
     {1,1,1,1,1,1,1,1},
@@ -23,12 +27,14 @@ int map[8][8] = {
 
 void drawPlayer(SDL_Renderer* renderer) {
     SDL_FRect rect = {
-        playerX, playerY,
+        playerX * CELL_W, playerY * CELL_W,
         10.0f, 10.0f
     };
     SDL_SetRenderDrawColor(renderer, 255, 255, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderFillRect(renderer, &rect);
-    SDL_RenderLine(renderer, playerX, playerY, 25*playerDirX+playerX, 25*playerDirY+playerY);
+    SDL_RenderLine(
+        renderer, playerX * CELL_W + 5, playerY * CELL_W + 5, 25 * playerDirX+playerX * CELL_W, 25 * playerDirY + playerY * CELL_W
+    );
 }
 
 void drawMap(SDL_Renderer* renderer) {
@@ -51,7 +57,7 @@ int main() {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("raycast", 800, 600, 0);
+    SDL_Window* window = SDL_CreateWindow("raycast", SCREEN_W, SCREEN_H, 0);
     if (!window) {
         SDL_Log("Failed to create window: %s", SDL_GetError());
         SDL_Quit();
@@ -61,15 +67,19 @@ int main() {
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
     SDL_SetRenderVSync(renderer, 1);
 
-    playerX = 400.0;
-    playerY = 300.0;
+    playerX = 3;
+    playerY = 4;
 
     playerDirX = 1.0;
     playerDirY = 0.0;
     playerRot = 0.0;
 
-    moveSpeed = 2;
-    rotationSpeed = 2;
+    planeX = 0;
+    planeY = 0.66; // 66 degrees FOV
+    planeRot = 0;
+
+    moveSpeed = 0.03;
+    rotationSpeed = 0.1;
 
     SDL_Event event;
     int running = 1;
@@ -80,26 +90,6 @@ int main() {
             case SDL_EVENT_QUIT:
                 running = 0;
                 break;
-            case SDL_EVENT_KEY_DOWN:
-                switch (event.key.key) {
-                case SDLK_W:
-                    playerX += playerDirX * moveSpeed;
-                    playerY += playerDirY * moveSpeed;
-                    break;
-                case SDLK_S:
-                    playerX -= playerDirX * moveSpeed;
-                    playerY -= playerDirY * moveSpeed;
-                    break;
-                case SDLK_A:
-                    playerRot += rotationSpeed;
-                    break;
-                case SDLK_D:
-                    playerRot -= rotationSpeed;
-                    break;
-                default:
-                    break;
-                }
-                break;
             default:
                 break;
             }
@@ -107,11 +97,103 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
         SDL_RenderClear(renderer);
 
-        playerDirX = sin(playerRot);
-        playerDirY = cos(playerRot);
+        const bool *keys = SDL_GetKeyboardState(NULL);
 
-        drawMap(renderer);
-        drawPlayer(renderer);
+        if(keys[SDL_SCANCODE_A]) {
+            double oldDirX = playerDirX;
+            playerDirX = playerDirX * cos(-rotationSpeed) - playerDirY * sin(-rotationSpeed);
+            playerDirY = oldDirX * sin(-rotationSpeed) + playerDirY * cos(-rotationSpeed);
+            double oldPlaneX = planeX;
+            planeX = planeX * cos(-rotationSpeed) - planeY * sin(-rotationSpeed);
+            planeY = oldPlaneX * sin(-rotationSpeed) + planeY * cos(-rotationSpeed);
+        }
+        if(keys[SDL_SCANCODE_D]) {
+            double oldDirX = playerDirX;
+            playerDirX = playerDirX * cos(rotationSpeed) - playerDirY * sin(rotationSpeed);
+            playerDirY = oldDirX * sin(rotationSpeed) + playerDirY * cos(rotationSpeed);
+            double oldPlaneX = planeX;
+            planeX = planeX * cos(rotationSpeed) - planeY * sin(rotationSpeed);
+            planeY = oldPlaneX * sin(rotationSpeed) + planeY * cos(rotationSpeed);
+        }
+
+        if (keys[SDL_SCANCODE_W]) {
+            if (!map[(int)(playerY + playerDirY * moveSpeed)][(int)playerX])
+                playerY += playerDirY * moveSpeed;
+            if (!map[(int)playerY][(int)(playerX + playerDirX * moveSpeed)])
+                playerX += playerDirX * moveSpeed;
+        }
+        if (keys[SDL_SCANCODE_S]) {
+            playerX -= playerDirX * moveSpeed;
+            playerY -= playerDirY * moveSpeed;
+        }
+
+        for(int x = 0; x < SCREEN_W; x++) {
+            double cameraX = 2.0 * (double)x / (double)SCREEN_W - 1;
+            double rayDirX = playerDirX + planeX * cameraX;
+            double rayDirY = playerDirY + planeY * cameraX;
+
+            int mapX = (int)playerX;
+            int mapY = (int)playerY;
+            double sideDistX, sideDistY;
+
+            double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
+            double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
+            double perpWallDist;
+
+            int stepX, stepY;
+
+            int hit = 0;
+            int side;
+
+            if(rayDirX < 0) {
+                stepX = -1;
+                sideDistX = (playerX - mapX) * deltaDistX;
+            } else {
+                stepX = 1;
+                sideDistX = (mapX + 1.0 - playerX) * deltaDistX;
+            }
+
+            if(rayDirY < 0) {
+                stepY = -1;
+                sideDistY = (playerY - mapY) * deltaDistY;
+            } else {
+                stepY = 1;
+                sideDistY = (mapY + 1.0 - playerY) * deltaDistY;
+            }
+
+            while(!hit) {
+                if (sideDistX < sideDistY) {
+                    sideDistX += deltaDistX;
+                    mapX += stepX;
+                    side = 0;
+                } else {
+                    sideDistY += deltaDistY;
+                    mapY += stepY;
+                    side = 1;
+                }
+
+                if(map[mapY][mapX]) hit = 1;
+            }
+
+            if(side == 0) perpWallDist = (sideDistX - deltaDistX);
+            else          perpWallDist = (sideDistY - deltaDistY);
+
+            int lineHeight = (int)(SCREEN_H / perpWallDist);
+            int lineStart = -lineHeight / 2 + SCREEN_H / 2;
+            if(lineStart < 0) lineStart = 0;
+            int lineEnd = lineHeight / 2 + SCREEN_H / 2;
+            if(lineEnd >= SCREEN_H) lineEnd = SCREEN_H - 1;
+            
+            int color = lineHeight * 0.5;
+            if (color < 0)   color = 0;
+            if (color > 255) color = 255;
+
+            SDL_SetRenderDrawColor(renderer, color, color, color, 255);
+            SDL_RenderLine(renderer, x, lineStart, x, lineEnd);
+        }
+
+        //drawMap(renderer);
+        //drawPlayer(renderer);
 
         SDL_RenderPresent(renderer);
 
